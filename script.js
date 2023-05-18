2aec9d52ab058b0986cdbe098b04b46b2c233b29
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-18 16:15:33 -0300

    requisito 7 concluido, criação evento click na lista de tarefas

--- script.js
+++ script.js
@@ -8,9 +8,19 @@ const addTarefa = () => {
     const inputTarefa = document.querySelector('#texto-tarefa');
     const liTarefa = document.createElement('li');
     liTarefa.innerText = inputTarefa.value;
+    liTarefa.classList.add('item-lista')
     inputTarefa.value = '';
     listaTarefas.appendChild(liTarefa);
   })
 };
 
+const efeitoClickTarefas = () => {
+  listaTarefas.addEventListener('click', (event) => {
+    if (event.target.classList.contains('item-lista')) {
+      event.target.classList.add('item-lista-clicado')
+    }
+  })
+}
+
 addTarefa();
+efeitoClickTarefas();
