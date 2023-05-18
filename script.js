4454236538f33595c8c1d0436f0ec809490e9ed2
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-18 17:40:33 -0300

    Requisito 9, função item concluido

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const listaTarefas = document.querySelector('#lista-tarefas');
-
+const classItemLista = 'item-lista';
+const classItemListaClicado = 'item-lista-clicado';
 
 const addTarefa = () => {
   const addBotao = document.querySelector('#criar-tarefa');
@@ -8,25 +9,38 @@ const addTarefa = () => {
     const inputTarefa = document.querySelector('#texto-tarefa');
     const liTarefa = document.createElement('li');
     liTarefa.innerText = inputTarefa.value;
-    liTarefa.classList.add('item-lista')
+    liTarefa.classList.add(classItemLista);
     inputTarefa.value = '';
     listaTarefas.appendChild(liTarefa);
-  })
+  });
 };
 
 const efeitoClickTarefas = () => {
   listaTarefas.addEventListener('click', (event) => {
-    if (event.target.classList.contains('item-lista')) {
+    if (event.target.classList.contains(classItemLista)) {
       const selectedItemlist = document.querySelector('#lista-tarefas .item-lista-clicado');
       if (selectedItemlist) {
-        selectedItemlist.classList.remove('item-lista-clicado');
-        event.target.classList.add('item-lista-clicado');
+        selectedItemlist.classList.remove(classItemListaClicado);
+        event.target.classList.add(classItemListaClicado);
       } else {
-        event.target.classList.add('item-lista-clicado');
+        event.target.classList.add(classItemListaClicado);
       }
     }
-  })
-}
+  });
+};
+
+const efeitoDbClickTarefas = () => {
+  listaTarefas.addEventListener('dblclick', (event) => {
+    if (event.target.classList.contains(classItemLista)) {
+      if (event.target.classList.contains('completed')) {
+        event.target.classList.remove('completed');
+      } else {
+        event.target.classList.add('completed');
+      }
+    }
+  });
+};
 
 addTarefa();
 efeitoClickTarefas();
+efeitoDbClickTarefas();
